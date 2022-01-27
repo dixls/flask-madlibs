acd0fe2eac63bd1d30592e1f3dff8f4827b73978
pixls <tha.pixls@gmail.com>
2022-01-27 17:52:42 -0500

    basic madlib functionality working

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from stories import Story, story
 
 app = Flask(__name__)
 
+@app.route('/')
+def madlib_form():
+
+    items = story.prompts
+
+    return render_template('madlib_form.html', words=items)
+
+@app.route('/completed_madlib')
+def madlip_gen():
+
+    answers = request.args
+
+    finished_story = story.generate(answers)
+
+    return render_template('completed_madlib.html', story=finished_story)
